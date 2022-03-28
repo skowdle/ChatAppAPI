2272cb4a4f4c705029f999d9876b179832963075
skowdle <72529256+skowdle@users.noreply.github.com>
2022-03-28 00:28:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,66 @@ app = Flask (__name__)
 userinfo = []
 keyinfo = {}
 
+class Message:
+    def __init__(self,user,msg):
+        self.user = user
+        self.msg = msg
+        
 class User:
     def __init__(self,username,password):
         self.username = username
         self.password = password
+        self.messages = []
+        self.friends = []   
 
 User1 = User("admin","password")
 userinfo.append(User1)  
 
+@app.route('/friend',methods=["POST"])
+def user_friend():
+    respkey = {}
+    givenuser = request.get_json()
+    if "username" not in givenuser or "loggedInUser" not in givenuser:
+        respkey["success"] = "false"
+        return jsonify(respkey)
+    for x in userinfo:
+        if givenuser["username"] == x.username:
+            for y in x.friends:
+                if y.username == givenuser["loggedInUser"]:
+                    respkey["success"] = "false"
+                    return jsonify(respkey)
+    for x in userinfo:
+        if givenuser["username"] == x.username:
+            for y in userinfo:
+                if givenuser["loggedInUser"] == y.username:
+                    y.friends.append(x)
+                    y.messages.append([])
+                    x.messages.append([])
+                    x.friends.append(y)
+                    respkey["success"] = "true"
+                    return jsonify(respkey)
+    respkey["success"] = "false"
+    return jsonify(respkey)
+    
+@app.route('/send',methods=["POST"])
+def user_message(): 
+    respkey = {}
+    givenuser = request.get_json()
+    if "username" not in givenuser or "loggedInUser" not in givenuser:
+        respkey["success"] = "false"
+        return jsonify(respkey)
+    for x in userinfo:
+        if givenuser["username"] == x.username:
+            for y in userinfo:
+                if givenuser["loggedInUser"] == y.username:
+                    y.friends.append(x)
+                    y.messages.append([])
+                    x.messages.append([])
+                    x.friends.append(y)
+                    respkey["success"] = "true"
+                    return jsonify(respkey)
+    respkey["success"] = "false"
+    return jsonify(respkey)
     
 @app.route('/login',methods=["POST"])
 def user_login():
